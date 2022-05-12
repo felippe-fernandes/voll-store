281433c506f7d91dda11b6cd1bad14455c8dd8cb
Felippe Fernandes <felippe1000@hotmail.com>
2022-05-12 12:26:01 -0300

    FEAT: Add new Route

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './Pages/Login/Login';
+import ProductList from './Pages/Product List/ProductList'
 import NotFound from './Pages/Not Found/NotFound'
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={ <Login/> } />
-      {/* <Route exact path="/foods" element={ Foods } />       */}
+      <Route exact path="/productlist" element={ <ProductList/> } />
       <Route path="*" element={ NotFound } />
     </Routes>
   );
