4d13183afe2d6671e318c93932a1abf367d69200
Felippe Fernandes <felippe1000@hotmail.com>
2022-05-13 13:12:46 -0300

    FEAT: Adding Card component to  the main page

--- src/Pages/Product List/ProductList.jsx
+++ src/Pages/Product List/ProductList.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import styles from './styles.module.css'
+import { getProducts } from '../../Services/Api';
+import RecipesContext from '../../Context/VollContext'
+import Card from '../../Components/Card/Card'
+
 function ProductList() {
+  const { productList, setProductList } = useContext(RecipesContext);
+
+  useEffect(() => {
+    const productListGenerator = async () => setProductList(await getProducts())
+    productListGenerator()
+  }, [setProductList])
 
   return (
-      <div>Teste</div>
+    <div className={styles.ProductListMainPage}>
+      <div className={styles.CardsContainer}>
+        {productList?.length > 0
+          && (
+            productList.map(({
+              id,
+              price,
+              title,
+              thumbnail,
+              shipping,
+              available_quantity: availableQuantity,
+            }) => (
+              <Card
+                key={ id }
+                cardName={ title }
+                cardPrice={ price }
+                cardImage={ thumbnail.replace('I.jpg', 'W.webp') }
+                id={ id }
+                availableQuantity={ availableQuantity }
+              />
+            )))}
+      </div>
+    </div>
   );
 }
 
